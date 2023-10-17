7605997907973a61ba2aa61316569ebe547b07ab
MANISHA155 <146463357+MANISHA155@users.noreply.github.com>
2023-10-17 02:30:33 -0700

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -4,3 +4,17 @@ def sum (a,b)
   echo "the sum of ${a} and ${b} is ${a+b}"
 }
 
+def sub(a,b)
+{
+  echo "the subtraction of ${a} and ${b} is ${a-b}"
+}
+
+def mul(a,b)
+{
+  echo "the product of ${a} and ${b} is ${a*b}"
+} 
+
+dev div (a,b)
+{
+  echo "the division of ${a} and ${b} is $ {a/b}"
+}
