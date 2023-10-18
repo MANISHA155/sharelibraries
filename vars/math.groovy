ac41fff50991dce760aa5357e8315314357b7337
MANISHA155 <146463357+MANISHA155@users.noreply.github.com>
2023-10-18 02:05:32 -0700

    Update math.groovy

--- vars/math.groovy
+++ vars/math.groovy
@@ -11,3 +11,10 @@ def newDeploy(workspace,ip,context)
 {
     sh "scp /var/lib/jenkins/workspace/${workspace}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
 }
+
+def exceuteselenium(jobname)
+{
+    sh ''java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar''
+}
+
+  
